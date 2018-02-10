15335bffa0291b25bdc852bda779cf48f4389427
gabrielhdt <gabrielhondet@gmail.com>
2018-02-10 17:50:04 +0100

    (+) added useful functions

--- crispy_chainshaw/db.cpp
+++ crispy_chainshaw/db.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
-#include <ctime>
-#include <QtSql>
+#include "db.h"
 
-class DbManager
+QString DATEFMT = "yyyy-mm-dd";
+
+WorkDayStatus str2wds(const QString& s)
 {
-	public:
-		DbManager(const QString& path);
-		bool add_species(const QString&);
-		bool consult();
-	private:
-		QSqlDatabase m_db;
-};
+	WorkDayStatus res;
+	if (s == "off") { res = WorkDayStatus::OFF; }
+	else if (s == "office") { res = WorkDayStatus::OFFICE; }
+	else if (s == "standby") { res = WorkDayStatus::STANDBY; }
+	else if (s == "v1") { res = WorkDayStatus::V1; }
+	else if (s == "v2") { res = WorkDayStatus::V2; }
+	else if (s == "v3") { res = WorkDayStatus::V3; }
+	else { throw; }
+	return res;
+}
 
 DbManager::DbManager(const QString& path)
 {
@@ -27,10 +30,9 @@ DbManager::DbManager(const QString& path)
 	}
 }
 
-bool DbManager::add_pilot(const QString& name, const QString& role,
-		const QInt freq_max)
+void DbManager::add_pilot(const QString& name, const QString& role,
+		const int& freq_max)
 {
-	bool success = false;
 	QSqlQuery query(m_db);
 	query.prepare(
 			"INSERT INTO Pnt (name, role, freq_max) VALUES " \
@@ -39,15 +41,56 @@ bool DbManager::add_pilot(const QString& name, const QString& role,
 	query.bindValue(":role", role);
 	query.bindValue(":freq_max", freq_max);
 	if (query.exec()) {
-		success = true;
 	}
 	else {
-		qDebug() << "error adding species: "
+		qDebug() << "error adding pilot: "
 			<< query.lastError();
 	}
-	return success;
 }
 
+void DbManager::add_acft_model(const QString& name, const int& freq_max,
+		const int& nop, const int& ntot)
+{
+	QSqlQuery query(m_db);
+	query.prepare(
+			"INSERT INTO Acft_model (name, freq_max, nop, ntot) VALUES " \
+			"(:name, :freq_max, :nop, :ntot)");
+	query.bindValue(":name", name);
+	query.bindValue(":freq_max", freq_max);
+	query.bindValue(":nop", nop);
+	query.bindValue(":ntot", ntot);
+	if (query.exec()) {
+	}
+	else {
+		qDebug() << "error adding acft model: "
+			<< query.lastError();
+	}
+}
+
+WorkDayStatus DbManager::see_status(const QDateTime date, const int pntid)
+{
+	QSqlQuery query(m_db);
+	query.prepare(
+			"SELECT Workday.status FROM Workday INNER JOIN Pnt ON " \
+			"Workday.pntid = Pnt.id WHERE " \
+			"Pnt.id = :pntid AND Workday.date = :date"
+			);
+	query.bindValue(":pntid", pntid);
+	query.bindValue(":date", date.toString(DATEFMT));
+	if (query.exec()) {
+		// Result should be unique
+		while (query.next()) {
+			auto res = query.value(0).toString();
+			return str2wds(res);
+		}
+	}
+	else {
+		qDebug() << "error consulting status: "
+			<< query.lastError();
+	}
+}
+
+/*
 bool DbManager::consult()
 {
 	bool success = false;
@@ -65,3 +108,4 @@ bool DbManager::consult()
 	}
 	return success;
 }
+*/
