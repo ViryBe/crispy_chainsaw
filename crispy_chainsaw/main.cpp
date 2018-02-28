70f2dcd64c40fc3a9a91c40fe18404ad1126cf7c
gabrielhdt <gabrielhondet@gmail.com>
2018-02-28 18:42:14 +0100

    (+) approximatively relative filepath for test

--- crispy_chainsaw/main.cpp
+++ crispy_chainsaw/main.cpp
@@ -6,8 +6,19 @@
 
 int main(int argc, char *argv[])
 {
-    DbManager db = DbManager("/home/gabriel/workspace/vcs/crispy_chainsaw/dummydata/dummy.db");
-    qDebug() << (db.test() ? "database tested succesfully" : "database testing failure");
+    QDir qdir = QDir(""); //Current path
+    qdir.cdUp();
+    qDebug() << qdir.currentPath();
+    if (qdir.cd(qdir.currentPath() + "/dummydata")) {
+        QString fpath = qdir.currentPath() + "dummy.db";
+        DbManager db = DbManager(fpath);
+        qDebug() << (db.test() ?
+                         "database tested succesfully" :
+                         "database testing failure");
+    }
+    else {
+        qDebug() << "database not tested, file path resolution failure";
+    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
