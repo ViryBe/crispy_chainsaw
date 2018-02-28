3f6f2c110b48cac7e0c445bedd2639cdf49ffb3c
Claire COUSIN <claire.cousin@alumni.enac.fr>
2018-02-28 19:22:23 +0100

    (+) Add new pilot complete

--- crispy_chainsaw/newpilot.cpp
+++ crispy_chainsaw/newpilot.cpp
@@ -12,3 +12,30 @@ newPilot::~newPilot()
 {
     delete ui;
 }
+
+void newPilot::on_buttonBox_accepted()
+{
+    QString code = ui -> codePilotEdit -> text();
+    code.toStdString();
+    QString name = ui -> namePilotEdit -> text();
+    name.toStdString();
+    QString function;
+    qDebug(code.toLatin1() +"\n" +name.toLatin1()); //ok
+    if (ui->B727Cdt->isChecked()){
+        function = "B727Cdt";
+    }
+    else if (ui->B737Cdt->isChecked()){
+        function = "B737Cdt";
+    }
+    else if (ui->B727FE->isChecked()){
+        function = "B72FE";
+    }
+    else if (ui->B727FO->isChecked()){
+        function = "B72FO";
+    }
+    else if (ui->B737FO->isChecked()){
+        function = "B73FO";
+    }
+    qDebug(function.toLatin1());
+
+}
