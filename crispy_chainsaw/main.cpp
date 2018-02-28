cf3677b71c689ea384e4e83942d8f933f4874e79
gabrielhdt <gabrielhondet@gmail.com>
2018-02-28 18:47:25 +0100

    (+) well tried but no

--- crispy_chainsaw/main.cpp
+++ crispy_chainsaw/main.cpp
@@ -6,6 +6,7 @@
 
 int main(int argc, char *argv[])
 {
+    /*
     QDir qdir = QDir(""); //Current path
     qdir.cdUp();
     qDebug() << qdir.currentPath();
@@ -19,6 +20,13 @@ int main(int argc, char *argv[])
     else {
         qDebug() << "database not tested, file path resolution failure";
     }
+    */
+    QString fpath = "/home/gabriel/workspace/vcs/"
+            "crispy_chainsaw/dummydata/dummy.db";
+    DbManager db = DbManager(fpath);
+    qDebug() << (db.test() ?
+                     "database tested succesfully" :
+                     "database testing failure");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
