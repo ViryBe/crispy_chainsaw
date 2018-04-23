771c9e122a37031e89473be2ea602b0cffc49593
Claire COUSIN <claire.cousin@alumni.enac.fr>
2018-04-23 17:36:02 +0200

    (+) Activation des champs si possible

--- crispy_chainsaw/modifyflight.cpp
+++ crispy_chainsaw/modifyflight.cpp
@@ -8,9 +8,48 @@ ModifyFlight::ModifyFlight(QDate dateSend, QString plane, QWidget *parent) :
     ui->setupUi(this);
     ui->dateEdit->setDate(dateSend);
     ui->time->setTime(time);
+    if (dateSend.currentDate() > dateSend){
+        ui->time->setEnabled(false);
+        ui->vol->setEnabled(false);
+        ui->time->setEnabled(false);
+        ui->pasEffectue->setChecked(true);
+    }
 }
 
 ModifyFlight::~ModifyFlight()
 {
     delete ui;
+}
+
+
+void ModifyFlight::on_pasEffectue_stateChanged(int arg1)
+{
+    if (ui->pasEffectue->isChecked()){
+        ui->time->setEnabled(false);
+        ui->vol->setEnabled(false);
+        ui->time->setEnabled(false);
+    }
+    else {
+        ui->time->setEnabled(true);
+        ui->vol->setEnabled(true);
+        ui->time->setEnabled(true);
+    }
+}
+
+void ModifyFlight::on_dateEdit_userDateChanged(const QDate &date)
+{
+    if (date > date.currentDate()){
+        ui->time->setEnabled(false);
+        ui->vol->setEnabled(false);
+        ui->time->setEnabled(false);
+        ui->pasEffectue->setChecked(true);
+        ui->pasEffectue->setEnabled(false);
+    }
+    else {
+        ui->time->setEnabled(true);
+        ui->vol->setEnabled(true);
+        ui->time->setEnabled(true);
+        ui->pasEffectue->setChecked(false);
+        ui->pasEffectue->setEnabled(true);
+    }
 }
--- crispy_chainsaw/modifyflight.h
+++ crispy_chainsaw/modifyflight.h
@@ -15,6 +15,12 @@ public:
     explicit ModifyFlight(QDate dateSend, QString plane,QWidget *parent = 0);
     ~ModifyFlight();
 
+private slots:
+
+    void on_pasEffectue_stateChanged(int arg1);
+
+    void on_dateEdit_userDateChanged(const QDate &date);
+
 private:
     QString appareil;
     Ui::ModifyFlight *ui;
